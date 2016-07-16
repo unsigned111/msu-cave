5308b70ff317281bfe474848dd8024c75de30c98
David Millman <dave@cs.unc.edu>
2016-07-16 18:32:14 -0400

    Hook everything up

--- broadcaster/main.js
+++ broadcaster/main.js
@@ -1,26 +1,62 @@
 /*jslint node: true, esversion: 6 */
 'use strict';
 
-var firebase = require('firebase');
+var http = require('http');
+var jsonBody = require('body/json');
+
+var validator = require('./validator');
+var broadcaster = require('./broadcaster');
 
 // TODO:DLM: bring in from command line
 let CREDENTIALS = '../credentials/msu-cave-f3ae939d1917.json';
 let DATABASE_URL = 'https://msu-cave.firebaseio.com';
+let PORT = 3000;
 
-var app = firebase.initializeApp({
-    serviceAccount: CREDENTIALS,
-    databaseURL: DATABASE_URL,
-});
-
+// TODO:DLM: the path is constructed from path concating the
+// installation and the headset id
 let path = 'test_data/yeah';
-var db = firebase.database();
-var ref = db.ref(path);
-ref.set({ test_data: 'some data' });
+let firebaseRef = new broadcaster.FirebaseRef(CREDENTIALS, DATABASE_URL);
+let firebaseBroadcaster = new broadcaster.Broadcaster(firebaseRef.database(), path);
 
-ref.on('value', function(snapshot) {
-  console.log(snapshot.val());
-});
+function echo(output) {
+  return function() { console.log(output); };
+}
 
-setTimeout(function() {
-    ref.set({ test_data: 'some new data' });
-}, 1000);
+function finalizeResponse(request, response, statusCode, body) {
+  response.statusCode = statusCode;
+  response.write(JSON.stringify(body));
+  console.log("Processed:" + request.url + " status:" + response.statusCode);
+  response.end();
+}
+
+function broadcast(request, response, body) {
+  firebaseBroadcaster.push(body);
+  finalizeResponse(request, response, 200, {});
+}
+
+function processValidBody(request, response, body) {
+  let validData = validator.validate(body);
+  if (! validData.success) {
+    finalizeResponse(request, response, 400, validData.response);
+  } else {
+    broadcast(request, response, body);
+  }
+}
+
+function handleRequest(request, response) {
+  function processBody(err, body) {
+    if (err) {
+      let responseBody = {
+        errorMessage: "Error processing body",
+        developer: err
+      };
+      finalizeResponse(request, response, 500, responseBody);
+    } else {
+      processValidBody(request, response, body);
+    }
+  }
+  jsonBody(request, response, processBody);
+}
+
+var server = http.createServer(handleRequest);
+server.listen(PORT, echo("Server init"));
