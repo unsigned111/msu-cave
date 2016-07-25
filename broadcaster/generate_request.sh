8ba5b408a775eb43ba39b436febb35c30ee480d2
David Millman <dave@cs.unc.edu>
2016-07-24 20:05:41 -0400

    Fix warning

--- broadcaster/generate_request.sh
+++ broadcaster/generate_request.sh
@@ -2,7 +2,7 @@
 address="localhost:3000"
 
 http "$address" delta:=1 theta:=2 loAlpha:=3 hiAlpha:=4 \
-    loBeta:=5 hiBeta:=6 loGamma:=7 midGamma:=$(date +%s)
+    loBeta:=5 hiBeta:=6 loGamma:=7 midGamma:="$(date +%s)"
 
 http "$address" delta:=1 theta:=2 loAlpha:=3 hiAlpha:=4 \
     hiBeta:=6 loGamma:=7 midGamma:=8
