b8d7243b8c2865ec8dc073c488cbf689bd17df2a
Chris Huvaere <chuvaere@montana.edu>
2017-07-06 20:48:34 -0600

    Added a flag to enable toggling of headset on/off messages.

--- osc_src/osc_src.go
+++ osc_src/osc_src.go
@@ -8,14 +8,16 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"strings"
 
 	"github.com/hypebeast/go-osc/osc"
 )
 
 type AppArgs struct {
-	ReplayFileName  string
-	Port            int
-        Delay           int
+	ReplayFileName  	string
+	Port			int
+	Delay		   	int
+	Toggle			bool
 }
 
 func parseArgs() AppArgs {
@@ -29,22 +31,29 @@ func parseArgs() AppArgs {
 		7770,
 		"Port to send data",
 	)
-        delay := flag.Int(
-                "d",
-                1,
-                "Delay between messages",
-        )
+	delay := flag.Int(
+		"d",
+		1,
+		"Delay between messages",
+	)
+	toggle := flag.Bool(
+		"t",
+		true,
+		"Toggle headset on/off enabled",
+	)
 	flag.Parse()
 
 	args := AppArgs{
-		ReplayFileName: *fileName,
-		Port:           *port,
-                Delay:          *delay,
+		ReplayFileName:	*fileName,
+		Port:		*port,
+		Delay:		*delay,
+		Toggle:		*toggle,
 	}
 
 	fmt.Println("Reading from:", args.ReplayFileName)
 	fmt.Println("Destination Port:", args.Port)
-        fmt.Println("Delay:", args.Delay)
+	fmt.Println("Delay:", args.Delay)
+	fmt.Println("Headset Toggle On/Off:", args.Toggle)
 	return args
 }
 
@@ -54,16 +63,30 @@ func check(e error) {
 	}
 }
 
+var args AppArgs
+var messageChannel = make(chan osc.Packet)
+
 func main() {
 	// parse the command line arguments
-	args := parseArgs()
+	args = parseArgs()
+
+	client := osc.NewClient("localhost", args.Port)
+	go eegSender()
+	go toggleSender()
+
+	for {
+		client.Send(<-messageChannel)
+	}
+}
+
+func eegSender() {
+	endpoint := "/eeg"
 
 	// open the file
 	file, err := os.Open(args.ReplayFileName)
 	check(err)
 	defer file.Close()
-
-	client := osc.NewClient("localhost", args.Port)
+	
 	reader := csv.NewReader(file)
 	// read header
 	reader.Read()
@@ -73,15 +96,33 @@ func main() {
 			break
 		}
 		check(err)
-		fmt.Println(record)
-
-		msg := osc.NewMessage("/eeg")
+		msg := osc.NewMessage(endpoint)
 		for value := range record {
 			i, err := strconv.Atoi(record[value])
 			check(err)
 			msg.Append(int32(i))
 		}
-		client.Send(msg)
+		messageChannel <- msg
+		fmt.Println(endpoint, ":\t", strings.Join(record, " "))
 		time.Sleep(time.Duration(args.Delay) * time.Second)
+	}
+}
+
+func toggleSender() {
+	endpoint := "/onoff"
+	onoff := false
+	for {
+		var value int
+		if onoff {
+			value = 1
+		} else {
+			value = 0
+		}
+		msg := osc.NewMessage(endpoint)
+		msg.Append(int32(value))
+		messageChannel <- msg
+		onoff = !onoff
+		fmt.Println(endpoint, ":\t", value)
+		time.Sleep(time.Duration(args.Delay * 10) * time.Second)
 	}
 }
