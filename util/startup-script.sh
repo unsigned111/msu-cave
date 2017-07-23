d0b3b7ec0919edd69e155210d2ace25cd5cdff5f
Brendan Kristiansen <brendankristiansen@gmail.com>
2017-07-23 13:52:27 -0600

    Update startu-script

--- util/startup-script.sh
+++ util/startup-script.sh
@@ -3,28 +3,11 @@
 
 #!/bin/bash
 
-#Get Script Updates
-cd ~/msu-cave/
-git pull
-
-mac=$(cat /sys/class/net/wlan0/address)
-pod=$(curl https://msu-cave.firebaseio.com/config/pi_list/$mac/pod.json)
-ip=$(echo $(curl https://msu-cave.firebaseio.com/config/pod_list/$pod/ip_address.json) | tr -d '"')
-
-if [ $(hostname -I) == $ip ]
+if [ $(hostname -I) == "10.0.1.5"]
 then
-	if [ $pod == "server" ]
-	then
-		echo "SSH Server Configured"
-	fi
-	echo "Network configuration correct."
-	echo "Starting Pod..."
+	#Start ssh stuff nonsense thing
+else
 	cd ~/msu-cave/
 	./start
-else
-	echo raspberry | sudo -kS ./util/generate_interface_file/generate-interface.sh
-	#echo "Replacing interfaces file"
-	#echo raspberry | sudo -kS mv interfaces /sys/network/interfaces
-	#echo "Rebooting..."
-	#echo raspberry | sudo -kS reboot
-fi
+fi	
+	
