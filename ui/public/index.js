231b22d9e8a69b9b6abf4f213f97f69f83ef69d8
Carie Pointer <cariepointer@yahoo.com>
2017-07-23 20:26:56 -0600

    Initialize firebase

--- ui/public/index.js
+++ ui/public/index.js
@@ -1,29 +1,44 @@
 var firebase= require('firebase');
 
+firebase.initializeApp({
+            apiKey: "AIzaSyBMcXd3cG8JoinbmykBtXyWTJZn9P-XYDY",
+            databaseURL: "https://msu-cave.firebaseio.com/",
+            authDomain: "msu-cave.firebaseapp.com",
+         });
+firebase.auth().signInWithEmailAndPassword("admin@holter.com", "msu-cave-rocks").catch(function(error) {
+            console.log("Add some things in here");
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+
 var ui = firebase.database().ref('installations/holter/ui');
 var off_button = ui.child('button01_off');
 var power_button = ui.child('button02_power');
 var shutdown = ui.child('pod-1/shutdown');
 
-off_button.set(false);
-power_button.set(false);
 
-//set all pod systems to false
-for (i = 1; i<=5; i++) {
-	ui.child('pod-'+i).set( {
-		'broadcaster' 	 : false,
-		'headset-bridge' : false,
-		'lighting' 		 : false,
-		'shutdown' 		 : false,
-		'sound' 		 : false
-	});
+//set all pods to false - for testing purposes 
+function setFalse() { 
+	for (i = 1; i<=5; i++) {
+		ui.child('pod-'+i).set( {
+			'broadcaster' 	 : false,
+			'headset-bridge' : false,
+			'lighting' 		 : false,
+			'shutdown' 		 : false,
+			'sound' 		 : false
+		});
+	}
 }
 
+function newChild() {
+	ui.child('pod-2').set( {
+	'shutdown' : false
+});
+}
 
 // call shutdown function when button01_off is true
 off_button.on("value", function(snap) {
 	if (snap.val() == true) {
-		console.log("true");
 		shutdown_procedure();
 	}
 });
@@ -31,10 +46,9 @@ off_button.on("value", function(snap) {
 // turn off pi when button02_power is true
 power_button.on("value", function(snap) {
 	if (snap.val() == true) {
-		console.log("poweroff true - turning off");
-
-		off_button.set(false);
+		console.log("power button true - powering off");
 		shutdown.set(false);
+		off_button.set(false);
 		power_button.set(false);
 		//execSync('shutdown -h now');
 	}
@@ -43,8 +57,9 @@ power_button.on("value", function(snap) {
 
 function shutdown_procedure() {
 
-	//TODO add shutdown stuffs
+	//add shutdown stuffs here
+	//
 
-	shutdown.set(true); //set to true if no errors come up
+	shutdown.set(true);
 }
 
